b3c860961234d6b40e05430b9ea3a1b28bb8c215
ritesh156ece <ritesh156ece@gmail.com>
2026-03-01 02:38:11 +0530

    initial commit

--- patient-service/src/main/java/com/pm/patientservice/PatientServiceApplication.java
+++ patient-service/src/main/java/com/pm/patientservice/PatientServiceApplication.java
@@ -1,12 +1,15 @@
 package com.pm.patientservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class PatientServiceApplication {
 
   public static void main(String[] args) {
+    log.info("Starting Patient Service Application...");
     SpringApplication.run(PatientServiceApplication.class, args);
   }
 }
