c23334f4031586c8b7b627aacb1d96aab855cca4
ritesh156ece <ritesh156ece@gmail.com>
2026-03-08 00:11:50 +0530

    billing service and Docker

--- billing-service/src/main/java/com/pm/billingservice/BillingServiceApplication.java
+++ billing-service/src/main/java/com/pm/billingservice/BillingServiceApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BillingServiceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(BillingServiceApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(BillingServiceApplication.class, args);
+  }
 }
--- billing-service/src/test/java/com/pm/billingservice/BillingServiceApplicationTests.java
+++ billing-service/src/test/java/com/pm/billingservice/BillingServiceApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BillingServiceApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
-
+  @Test
+  void contextLoads() {}
 }
