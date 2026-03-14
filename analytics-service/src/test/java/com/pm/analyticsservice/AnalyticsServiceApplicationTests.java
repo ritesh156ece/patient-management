6b186402ff4e397ce2c1aedfd9c50bf74680c011
ritesh156ece <ritesh156ece@gmail.com>
2026-03-14 16:02:28 +0530

    kafka

---
+++ analytics-service/src/test/java/com/pm/analyticsservice/AnalyticsServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.pm.analyticsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticsServiceApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
